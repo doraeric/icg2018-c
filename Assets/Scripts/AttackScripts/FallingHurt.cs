f824ec97553b71f2745b3fa9f492c5b536568e9e
doraeric <benson.doraemon@gmail.com>
2018-06-11 04:56:49 +0800

    fix falling hurt

--- Assets/Scripts/AttackScripts/FallingHurt.cs
+++ Assets/Scripts/AttackScripts/FallingHurt.cs
@@ -3,7 +3,7 @@
 public class FallingHurt : NatureHurt {
 	[SerializeField]float heightTooLow;
 
-	bool hurtCondition() {
+	override protected bool hurtCondition() {
 		return gameObject.transform.position.y < heightTooLow;
 	}
 }
--- Assets/Scripts/AttackScripts/NatureHurt.cs
+++ Assets/Scripts/AttackScripts/NatureHurt.cs
@@ -1,25 +1,22 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerHealth))]
-public class NatureHurt : MonoBehaviour {
+public abstract class NatureHurt : MonoBehaviour {
 	[SerializeField]protected float hurtSec = 1f;
 	[SerializeField]protected int hurtDamage = 1;
-	protected bool hurtCondition {
-		get { return true; }
-	}
+	protected abstract bool hurtCondition();
 
 	PlayerHealth playerHealth;
 	Coroutine hurtCoroutine;
 
 	public void Awake() {
-		playerHealth = GetComponent<PlayerHealth>();
+		playerHealth = GameManager.Instance.LocalPlayer.GetComponent<PlayerHealth>();
 	}
 
 	public void Update() {
-		if (hurtCondition && hurtCoroutine == null) {
+		if (hurtCondition() && hurtCoroutine == null) {
 			hurtCoroutine = StartCoroutine(TakeDamege());
-		} else if (!hurtCondition) {
+		} else if (!hurtCondition()) {
 			StopCoroutine(hurtCoroutine);
 			hurtCoroutine = null;
 		}
