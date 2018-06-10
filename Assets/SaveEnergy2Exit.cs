ac20f9b9e74c18d6906806a46638f463df4dbd10
Rainforest <>
2018-06-11 06:57:16 +0800

    add enermy to Scene2 Add Victory end

---
+++ Assets/SaveEnergy2Exit.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveEnergy2Exit : MonoBehaviour {
+
+	public bool inCollider = false;
+	public float saveRate = 1.0f;
+	public float lossRate = -0.5f;
+
+	private float currentRate;		
+	public float currentEnergy = 0f;
+	public float EnerCapacity = 100f;
+
+	public GameObject portalEffect;
+	public ParticleSystem[] Circle_H;
+	public ParticleSystem Circle_V;
+
+	public ParticleSystem pointlight;
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentEnergy >= EnerCapacity){
+			SceneManager.LoadScene("BeginScene");
+			Debug.Log("loadScene");
+		}else if (currentEnergy < 0){
+			currentEnergy = 0;
+		}else{
+			if (inCollider){
+				currentEnergy += saveRate * Time.deltaTime;
+				Debug.Log(currentEnergy);
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.tag == "Player"){
+			Debug.Log("is Trigger");
+			inCollider = true;
+			currentRate = saveRate;
+		}
+	}
+
+	void OnTriggerExit (Collider other){
+		if (other.tag == "Player"){
+			Debug.Log("exit Trigger");
+			inCollider = false;
+			currentRate = lossRate;
+		}
+	}
+}
