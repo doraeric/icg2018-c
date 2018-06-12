4a5d261af67ad1b3de9a3bb3266c338617807a4d
Rainforest <>
2018-06-12 13:13:09 +0800

    edit Begin Scene Timelines

---
+++ Assets/SwitchCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour {
+
+	public Cinemachine.CinemachineBrain CameraManager;
+	public Animator CameraManagerAnimator;
+
+	public Cinemachine.CinemachineTrackedDolly cam1;
+
+	public Cinemachine.CinemachineTrackedDolly cam2;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
